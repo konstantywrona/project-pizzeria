c89772e0944cf8fbad52f6e78929978765deef5a
Konstanty Wrona <koswro@gmail.com>
2023-01-19 01:07:59 +0100

    Add server config

--- src/js/settings.js
+++ src/js/settings.js
@@ -121,7 +121,10 @@ export const settings = {
     id: 1,
   },
   db: {
-    url: '//localhost:3131',
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
